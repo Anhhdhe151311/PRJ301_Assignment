1d8f3dcb6bf54eebd69c59d30ea6ee01743ffb63
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-10-12 22:50:24 +0700

    add new Dbcontext

--- Assignment/src/java/Dal/DBContext.java
+++ Assignment/src/java/Dal/DBContext.java
@@ -1,32 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DBContext {
+/**
+ *
+ * @author Ngo Tung Son
+ */
+public abstract class DBContext<T> {
     protected Connection connection;
     public DBContext()
     {
-        //@Students: You are allowed to edit user, pass, url variables to fit 
-        //your system configuration
-        //You can also add more methods for Database Interaction tasks. 
-        //But we recommend you to do it in another class
-        // For example : StudentDBContext extends DBContext , 
-        //where StudentDBContext is located in dal package, 
         try {
             String user = "sa";
             String pass = "123456";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=master";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=PRJ301_Slot3Day246";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException | SQLException ex) {
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public static void main(String[] args) {
-        System.out.println(new DBContext().connection);
-    }
+    public abstract void insert(T model); // INSERT INTO
+    public abstract void update(T model); // UPDATE SET
+    public abstract void delete(T model); //DELETE FROM 
+    public abstract T get(int id);  //SELECT * FROM WHERE id = id
+    public abstract ArrayList<T> list();//SELECT * FROM
 }
