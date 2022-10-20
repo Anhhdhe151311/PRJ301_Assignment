e87b267cfcdc266fd83332e44344b230f6bca3d4
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-10-20 16:34:21 +0700

    Update TimeSlot.java

--- Assignment/src/java/Model/TimeSlot.java
+++ Assignment/src/java/Model/TimeSlot.java
@@ -4,10 +4,44 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
  */
 class TimeSlot {
+    private int tid;
+    private String dascription;
+    
+    private ArrayList<Session> sessions = new ArrayList<>();
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getDascription() {
+        return dascription;
+    }
+
+    public void setDascription(String dascription) {
+        this.dascription = dascription;
+    }
+
+    public ArrayList<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(ArrayList<Session> sessions) {
+        this.sessions = sessions;
+    }
+    
+    
+    
+    
     
 }
