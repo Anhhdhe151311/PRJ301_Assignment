34416300c6e69a0d8fa3e060a9946a16174a9003
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-10-20 16:26:40 +0700

    create and add data Session.java

--- Assignment/src/java/Model/Session.java
+++ Assignment/src/java/Model/Session.java
@@ -4,10 +4,101 @@
  */
 package Model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
  */
 class Session {
+    private int sesid;
+    private Group group;
+    private Room room;
+    private Date date;
+    private TimeSlot timeSlot;
+    private int index;
+    private Lecturer lecturer;
+    private boolean attanded;
+    private  ArrayList<Attandance> atts = new ArrayList<>();
+
+    
+    
+    
+    
+    public int getSesid() {
+        return sesid;
+    }
+
+    public void setSesid(int sesid) {
+        this.sesid = sesid;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public TimeSlot getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(TimeSlot timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public boolean isAttanded() {
+        return attanded;
+    }
+
+    public void setAttanded(boolean attanded) {
+        this.attanded = attanded;
+    }
+
+    public ArrayList<Attandance> getAtts() {
+        return atts;
+    }
+
+    public void setAtts(ArrayList<Attandance> atts) {
+        this.atts = atts;
+    }
+    
+    
+    
     
 }
