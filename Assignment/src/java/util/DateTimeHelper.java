68bf24c4da4dd45765926dd2cae51088bbec2450
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-10-28 17:52:12 +0700

    Update DateTimeHelper.java
    tự chỉnh sửa cho bản thân

--- Assignment/src/java/util/DateTimeHelper.java
+++ Assignment/src/java/util/DateTimeHelper.java
@@ -12,7 +12,7 @@ import java.util.Date;
 
 /**
  *
- * @author Ngo Tung Son
+ * @author Hà Duy Anh
  */
 public class DateTimeHelper {
 
@@ -29,25 +29,26 @@ public class DateTimeHelper {
         cal.set(Calendar.MILLISECOND, 0);
         return cal.getTime();
     }
+    // đăt time xuống 0 để chuyển đổi
 
     public static java.util.Date toDateUtil(java.sql.Date d) {
         java.util.Date x = new java.util.Date(d.getTime());
         x = removeTime(x);
         return x;
     }
-
+    //chuyển date từ sql sang util
     public static java.sql.Date toDateSql(java.util.Date d) {
         d= removeTime(d);
         return new java.sql.Date(d.getTime());
     }
-
+    // chuyển từ date util sang sql
     public static int getDayofWeek(java.util.Date d) {
         Calendar c = Calendar.getInstance();
         c.setTime(d);
-        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);//lấy đây là ngày mấy trong tuần
         return dayOfWeek;
     }
-    
+    // ngày thứ mấy trong tuần 
     public static Date addDays(java.util.Date d, int days)
     {
         Calendar cal = Calendar.getInstance(); 
@@ -55,7 +56,7 @@ public class DateTimeHelper {
         cal.add(Calendar.DATE, days);
         return cal.getTime();
     }
-    
+    //thêm ngày (cách số mấy trong dòng)
     public static ArrayList<java.sql.Date> 
         getDateList(java.sql.Date from, java.sql.Date to)
     {
@@ -73,6 +74,7 @@ public class DateTimeHelper {
         }
         return dates;
     }
+        //trả 1 list các nagyf từ from-to
     
     public static String getDayNameofWeek(java.sql.Date s) {
         java.util.Date d = toDateUtil(s);
@@ -91,7 +93,7 @@ public class DateTimeHelper {
         }
         return "Error";
     }
-    
+    // trả 1 string name day
     public static int compare(java.sql.Date a, java.sql.Date b)
     {
        
