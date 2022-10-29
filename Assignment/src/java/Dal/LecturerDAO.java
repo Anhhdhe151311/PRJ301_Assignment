b337ffc108b6efb75bf30abd3ff1ff1b307d7424
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-10-29 16:25:32 +0700

    add getIdNameLecturer(int lid)

--- Assignment/src/java/Dal/LecturerDAO.java
+++ Assignment/src/java/Dal/LecturerDAO.java
@@ -4,10 +4,16 @@
  */
 package Dal;
 
+import Model.Lecturer;
+
 /**
  *
  * @author Admin
  */
 public class LecturerDAO extends DBContext{
+
+    public static Lecturer getIdNameLecturer(int lid) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
