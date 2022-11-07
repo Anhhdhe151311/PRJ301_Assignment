f6f8807caa927746f369ec9060b6f88cda556523
Anhhdhe151311 <97275458+Anhhdhe151311@users.noreply.github.com>
2022-11-07 16:26:18 +0700

    Create AttandanceDAO.java

---
+++ Assignment/src/java/Dal/AttandanceDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dal;
+
+import Model.Attandance;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class AttandanceDAO extends DBContext{
+
+    public ArrayList<Attandance> getAttsBySessionId(int sesid) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
